bc1154471f640445db256955ae8fb26c993cd273
mma <111668501+mmalkindi@users.noreply.github.com>
2025-02-21 11:02:51 +0700

    Add second level and end screen

--- scenes/ObjectiveArea.gd
+++ scenes/ObjectiveArea.gd
@@ -2,4 +2,6 @@ extends Area2D
 
 func _on_ObjectiveArea_body_entered(body: RigidBody2D):
 	if (body.name == "BlueShip"):
-		print("Reached objective!")
+		get_tree().change_scene_to_file("res://scenes/SecondLevel.tscn")
+	elif (body.name == "GreenShip"):
+		get_tree().change_scene_to_file("res://scenes/GameEnd.tscn")
